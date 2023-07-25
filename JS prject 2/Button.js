b48d09d14d1e9f087f3e8acd99d1792394fa273d
Huraira Imtiaz <91866100+HurairaImtiaz@users.noreply.github.com>
2023-07-25 18:54:14 +0500

    added more buttons (email, start playing )

--- JS prject 2/Button.js
+++ JS prject 2/Button.js
@@ -6,3 +6,10 @@ document.getElementById("btnName").onclick = function(){
 document.getElementById("btn1").onclick = function(){
     alert("Your Name is : "+myName)
 }
+let myEmail="";
+document.getElementById("btnEmail").onclick = function(){
+    myEmail = prompt("Write your Email Plaese!") 
+}
+document.getElementById("btn2").onclick = function(){
+    alert("Your Email is : "+myEmail)
+}
